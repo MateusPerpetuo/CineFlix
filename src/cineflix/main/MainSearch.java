b4ef22d304d023323ffa2b8a7a42ef39fd1eff2b
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-05-06 05:10:36 -0300

    V0.003 - Configurando biblioteca Gson

--- src/cineflix/main/MainSearch.java
+++ src/cineflix/main/MainSearch.java
@@ -1,17 +1,14 @@
 package cineflix.main;
 import cineflix.models.Movie;
 import cineflix.models.MovieOmdb;
-import cineflix.models.Title;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MainSearch {
@@ -23,39 +20,40 @@ public class MainSearch {
 
         String adress = "http://www.omdbapi.com/?t=" + search + "&apikey=d346a2b7";
 
-        // Aqui estamos pedindo uma informação para a API
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(adress))
-                .build();
-
-        // Resposta da API
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-       //  System.out.println(response.body());
-
-        String json = response.body();
-        System.out.println(json);
-
-        // Crianto o gson e patronizando pra tudo minustulo
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-                .create();
-
-        MovieOmdb myMovieOmdb = gson.fromJson(json, MovieOmdb.class);
-        System.out.println("titulo: " + myMovieOmdb);
-
         try {
+            // Aqui estamos pedindo uma informação para a API
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(adress))
+                    .build();
+
+            // Resposta da API
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            //  System.out.println(response.body());
+
+            String json = response.body();
+            System.out.println(json);
+
+            // Criando o gson e padronizando pra tudo minusculo
+            Gson gson = new GsonBuilder()
+                    .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                    .create();
+
+            MovieOmdb myMovieOmdb = gson.fromJson(json, MovieOmdb.class);
+            System.out.println("titulo: " + myMovieOmdb);
+
+
             Movie myMovie = new Movie(myMovieOmdb);
             System.out.println(myMovie);
 
-        } catch (NumberFormatException e){
-            System.out.println("Aconteceu um erro: " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Aconteceu um erro de formatação e número: " + e.getMessage());
 
+        } catch (IllegalArgumentException e) {
+            System.out.println("Aconteceu um erro de argumento na busca");
         }
-
-
     }
 
 }
