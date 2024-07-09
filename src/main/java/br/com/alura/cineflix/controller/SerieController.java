ce5191ab0051127538fe22f12cccfde3fa11fe1c
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-09 01:03:28 -0300

    v0.300 - Inicio da configuração do projeto para uma API WEB com o Spring

--- src/main/java/br/com/alura/cineflix/controller/SerieController.java
+++ src/main/java/br/com/alura/cineflix/controller/SerieController.java
@@ -1,13 +1,21 @@
 package br.com.alura.cineflix.controller;
 
+import br.com.alura.cineflix.model.Serie;
+import br.com.alura.cineflix.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository serieRepository;
+
     @GetMapping("/series")
-    public String obterSeries () {
-        return "aqui vão ser listadas as séries";
+    public List<Serie> obterSeries () {
+        return serieRepository.findAll();
     }
 }
