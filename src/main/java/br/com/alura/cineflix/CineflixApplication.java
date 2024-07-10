cbf7f5e2151ec25ec182645a1df8c2b168ab3275
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-07-10 17:43:29 -0300

    v01.000 - Projeto finalizado

--- src/main/java/br/com/alura/cineflix/CineflixApplication.java
+++ src/main/java/br/com/alura/cineflix/CineflixApplication.java
@@ -1,9 +1,5 @@
 package br.com.alura.cineflix;
 
-import br.com.alura.cineflix.principal.Principal;
-import br.com.alura.cineflix.repository.SerieRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
